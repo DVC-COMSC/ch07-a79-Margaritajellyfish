724f887d08389216cd9311df919ecec7c7103e76
Wang, ChenYu git config --global  git config --global user.email <cwang929@insite.4cd.edu>
2022-10-16 22:25:18 -0700

    7-9

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ numbers = [     [99, 11, 66, 86, 55],
 
 rnum = len(numbers)
 cnum = len(numbers[0])
-
+max_roll = 0
+max_num = 0
 print ('Sum of rows: ', end=' ')
 for i in range(rnum):
-	rsum = sum(numbers[i])
-	print (rsum, end=' ')
+    rsum = sum(numbers[i])
+    if rsum > max_roll:
+        max_roll = rsum 
+        great_roll = numbers.index(numbers[i])
+    print(rsum, end=' ')
+    
 print()
+
 
 # ******************************
 # Make your Code
@@ -21,4 +27,9 @@ for j in range(cnum):
     csum = 0
     for k in range(rnum):
         csum += numbers[k][j]
+        if numbers[k][j] > max_num:
+            max_num = numbers[k][j]
     print (csum, end=' ')
+print()
+print(f"The roll that has the greatest sum:{great_roll} ")
+print(f"The greatest number: {max_num}")
